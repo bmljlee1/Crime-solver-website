6021a943a82e66b853f75fec8183316911a049a7
Jonathan Lee <kutsu89@gmail.com>
2024-09-29 16:19:30 +0100

    url change

--- client/src/api/ApiTheories.jsx
+++ client/src/api/ApiTheories.jsx
@@ -1,4 +1,4 @@
-const API_URL = "https://week07-project-1.onrender.com";
+const API_URL = "https://week07-project-yykp.onrender.com";
 // const API_URL = "http://localhost:8080";
 
 export const getAllTheories = async () => {
